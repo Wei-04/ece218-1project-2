99fdcfa49b98af1fa5db0b0a7ac8e8b4ceed57a9
Wei-04 <weihenghe04@gmail.com>
2024-01-24 21:09:14 +0000

    commit 1 (init)

---
+++ main.cpp
@@ -0,0 +1,47 @@
+/*! @mainpage TO BE INCLUDED****
+* @date Monday, January 24, 2024
+* @authors Louis Rahilly, Weiheng He
+* @section genDesc General Description
+*
+* DESCRIPTION TO BE INCLUDED*****
+* 
+*
+* @section changelog Changelog
+* | Date | Description |
+* |:----------:|:-----------------------------------------------|
+* | 01/24/2024 | First version of program |
+* 
+*
+*
+*/
+
+//=====[Libraries]=============================================================
+
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+//=====[Declaration and initialization of public global objects]===============
+
+
+
+//=====[Declaration and initialization of public global variables]=============
+
+
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+
+
+//=====[Main function, the program entry point after power on or reset]========
+
+int main()
+{
+    inputsInit();
+    outputsInit();
+    while (true) {
+
+        }
+}
+
+
+//=====[Implementation of global functions]====================================
