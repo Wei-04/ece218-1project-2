ef79f74745deb17db6b487fcba43067d5208c0b3
Wei-04 <weihenghe04@gmail.com>
2024-01-26 12:58:50 +0000

    commit 2 (Added framework for initialization and logic)

--- main.cpp
+++ main.cpp
@@ -3,15 +3,15 @@
 * @authors Louis Rahilly, Weiheng He
 * @section genDesc General Description
 *
-* DESCRIPTION TO BE INCLUDED*****
-* 
+* This is an implementation of a headlight control system to be used in a 1995 McLaren F1 LM.
+* The driver will have the ability to select on, off, or automatic headlights.
 *
 * @section changelog Changelog
 * | Date | Description |
 * |:----------:|:-----------------------------------------------|
 * | 01/24/2024 | First version of program |
+* | 01/25/2024 | Added framework for initialization and logic |
 * 
-*
 *
 */
 
@@ -22,7 +22,13 @@
 
 //=====[Declaration and initialization of public global objects]===============
 
+DigitalIn driverOccupancy(D2); // Object associated driver seat contact
+DigitalIn ignitionButton(BUTTON1); // Object associated with attempted ignition
+DigitalOut ignitionLED(LED2); // Object associated with sucessful igntion
+DigitalOut headlightLED(PE_14); //Object associated with headlights
 
+AnalogIn potentiometer(A0); //Potentiometer reading, used in selection of headlight mode 
+AnalogIn lightLevel(A2); //Light sensitive resistor reading, used to determine presence of daylight
 
 //=====[Declaration and initialization of public global variables]=============
 
@@ -30,7 +36,20 @@
 
 //=====[Declarations (prototypes) of public functions]=========================
 
+void inputsInit(); /*Initalizes inputs:
+driverOccupancy = OFF
+*/
 
+void outputsInit(); /*Initalizes outputs:
+ignitionLED = OFF
+headlightLED = OFF
+*/
+
+void engineUpdate(); //controls the engine state, returns the current engine state
+
+void headlightsUpdate(); //controls the headlights state based on the headlight mode
+
+UnbufferedSerial uartUsb(USBTX, USBRX, 115200); // Object associated with the uart process
 
 //=====[Main function, the program entry point after power on or reset]========
 
@@ -39,9 +58,31 @@ int main()
     inputsInit();
     outputsInit();
     while (true) {
-
+        engineUpdate();
         }
 }
 
 
 //=====[Implementation of global functions]====================================
+
+void inputsInit() {
+    driverOccupancy.mode(PullDown);
+}
+
+void outputsInit() {
+    ignitionLED = OFF;
+    headlightLED = OFF;
+}
+
+void engineUpdate() {
+    if (ignitionButton) {
+        uartUsb.write("if\r\n", 4);
+        if (driverOccupancy) {
+            ignitionLED = ON;
+        }
+        else {
+            uartUsb.write("else\r\n", 6);
+            ignitionLED = OFF;
+        }
+    }
+}
